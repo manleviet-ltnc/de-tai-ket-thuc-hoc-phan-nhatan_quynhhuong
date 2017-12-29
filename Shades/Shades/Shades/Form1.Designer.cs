ac3ad8331811ad5b5e9c6f90c1fc699812c56aea
phamhongnhatan <nhatanphamhong@gmail.com>
2017-12-29 10:06:14 +0700

    shades

--- Shades/Shades/Shades/Form1.Designer.cs
+++ Shades/Shades/Shades/Form1.Designer.cs
@@ -30,18 +30,239 @@
         {
             this.components = new System.ComponentModel.Container();
             this.tmrMoving = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // tmrMoving
+            // label1
             // 
-            this.tmrMoving.Enabled = true;
-            this.tmrMoving.Tick += new System.EventHandler(this.tmrMoving_Tick);
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(72, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(132, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(192, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(12, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(72, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(132, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(192, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Location = new System.Drawing.Point(12, 103);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 25);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Location = new System.Drawing.Point(72, 103);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(54, 25);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Location = new System.Drawing.Point(132, 103);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(54, 25);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label12.Location = new System.Drawing.Point(192, 103);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 25);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label13.Location = new System.Drawing.Point(12, 138);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(54, 25);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "label13";
+            // 
+            // label14
+            // 
+            this.label14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label14.Location = new System.Drawing.Point(72, 138);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(54, 25);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "label14";
+            // 
+            // label15
+            // 
+            this.label15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label15.Location = new System.Drawing.Point(132, 138);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(54, 25);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "label15";
+            // 
+            // label16
+            // 
+            this.label16.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label16.Location = new System.Drawing.Point(192, 138);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(54, 25);
+            this.label16.TabIndex = 15;
+            this.label16.Text = "label16";
+            // 
+            // label17
+            // 
+            this.label17.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label17.Location = new System.Drawing.Point(12, 175);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(54, 25);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "label17";
+            // 
+            // label18
+            // 
+            this.label18.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label18.Location = new System.Drawing.Point(72, 175);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(54, 25);
+            this.label18.TabIndex = 17;
+            this.label18.Text = "label18";
+            // 
+            // label19
+            // 
+            this.label19.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label19.Location = new System.Drawing.Point(132, 175);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(54, 25);
+            this.label19.TabIndex = 18;
+            this.label19.Text = "label19";
+            // 
+            // label20
+            // 
+            this.label20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label20.Location = new System.Drawing.Point(192, 175);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(54, 25);
+            this.label20.TabIndex = 19;
+            this.label20.Text = "label20";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Shades
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Shades";
             this.Text = "Shades";
@@ -54,6 +275,27 @@
         #endregion
 
         private System.Windows.Forms.Timer tmrMoving;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Shades/Shades/Shades/Form1.cs
+++ Shades/Shades/Shades/Form1.cs
@@ -18,77 +18,76 @@ namespace Shades
         public int cot = -1;
         public int hang = 0;
 
+        Label[,] A = new Label[5, 4];
 
-        enum Position
-        {
-            Left, Right, Up, Down
-        }
 
-        private int _x;
-        private int _y;
-        private Position _objPosition;
+        //enum Position
+        //{
+        //    Left, Right, Up, Down
+        //}
+
+        //private int _x;
+        //private int _y;
+        //private Position _objPosition;
 
         public Shades()
         {
             InitializeComponent();
 
-            _x = 50;
-            _y = 50;
-            _objPosition = Position.Down;
+            //_x = 50;
+            //_y = 50;
+            //_objPosition = Position.Down;
+
+            A[0, 0] = label1;
+            A[0, 1] = label2;
+            A[0, 2] = label3;
+            A[0, 3] = label4;
+            A[1, 0] = label5;
+            A[1, 1] = label6;
+            A[1, 2] = label7;
+            A[1, 3] = label8;
+            A[2, 0] = label9;
+            A[2, 1] = label10;
+            A[2, 2] = label11;
+            A[2, 3] = label12;
+            A[3, 0] = label13;
+            A[3, 1] = label14;
+            A[3, 2] = label15;
+            A[3, 3] = label16;
+            A[4, 0] = label17;
+            A[4, 1] = label18;
+            A[4, 2] = label19;
+            A[4, 3] = label20;
+
+            for (int i = 0; i < So_Hang; i++)
+                for (int j = 0; j < So_Cot; j++)
+                    A[i, j].Text = "" + 0;
+            timer1.Enabled = true;
         }
 
         private void Shades_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.FillRectangle(Brushes.Red, _x, _y, 70, 50);
+            //e.Graphics.FillRectangle(Brushes.Red, _x, _y, 70, 50);
 
-
+            Color color = Color.Gray;
+            for (int i = 0; i < So_Hang; i++)
+            {
+                for (int j = 0; j < So_Cot; j++)
+                {
+                    switch (A[i, j].Text)
+                    {
+                        case "0": color = Color.Gray; break;
+                        case "1": color = Color.Yellow; break;
+                        case "2": color = Color.Green;break;
+                        case "4": color = Color.Red;break;
+                    }
+                    A[i, j].BackColor = color;
+                }
+            }
         }
 
         private void Shades_Load(object sender, EventArgs e)
         {
-            int[,] A = new int[So_Hang, So_Cot];
-
-            for (int i = 0; i < So_Hang; i++)
-                for (int j = 0; j < So_Cot; j++)
-                    A[i, j] = 0;
-            while (true)
-            {
-                if (chamday)
-                {
-                    So_Cot = Them0();
-                    So_Hang = 0;
-                    chamday = false;
-                    Draw();
-                }
-                else
-                {
-                    if (hang == So_Hang - 1)
-                    {
-                        chamday = true;
-                        continue;
-                    }
-                    else
-                    {
-                        if (A[hang, cot] != 4 && A[hang + 1, cot] == A[hang, cot])
-                        {
-                            A[hang + 1, cot] += A[hang, cot];
-                            A[hang, cot] = 0;
-                            hang = hang + 1;
-                        }
-                        else if (A[hang + 1, cot] == 0)
-                        {
-                            A[hang + 1, cot] = A[hang, cot];
-                            A[hang, cot] = 0;
-                            hang = hang + 1;
-                        }
-                        else
-                            chamday = true;
-                    }
-                    Invalidate();
-                    
-                }
-                Invalidate();
-            }
             
         }
 
@@ -100,27 +99,23 @@ namespace Shades
                     DayHangXuong(i);
 
             //for (int j =0; j < Cols; j++)
-            Invalidate();
         }
 
         private void DayHangXuong(int i)
         {
-            int[,] A = new int[So_Hang, So_Cot];
             for (int k = i; k > 0; k--)
                 for (int j = 0; j < So_Cot; j++)
-                    A[k, j] = A[k - 1, j];
+                    A[k, j].Text = A[k - 1, j].Text;
             for (int j = 0; j < So_Cot; j++)
-                A[0, j] = 0;
-            Invalidate();
+                A[0, j].Text = "" + 0;
         }
 
         private bool KTHang(int i)
         {
-            int[,] A = new int[So_Hang, So_Cot];
-            int tam = A[i, 0];
+            int tam = int.Parse(A[i, 0].Text);
             if (tam == 0) return false;
             for (int j = 1; j < So_Cot; j++)
-                if (A[i, j] != tam)
+                if (int.Parse(A[i, j].Text) != tam)
                     return false;
             return true;
             
@@ -128,74 +123,108 @@ namespace Shades
 
         public int Them0()
         {
-            int[,] A = new int[So_Hang, So_Cot];
             Random r = new Random();
             int j = r.Next(0, 4);
 
-            A[0, j] = 1;
+            A[0, j].Text = "" + 1;
             return j;
             
         }
 
-        private void Draw()
+        //private void Draw()
+        //{
+            
+        //    Invalidate();
+        //}
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            for (int i = 0; i < So_Hang; i++)
+            if (chamday)
             {
-                for (int j = 0; j < So_Cot; j++) ;
+                cot = Them0();
+                hang = 0;
+                chamday = false;
             }
+            else
+            {
+                if (hang == So_Hang - 1)
+                {
+                    chamday = true;
+                }
+                else
+                {
+                    if (A[hang, cot].Text != "4" && A[hang + 1, cot].Text == A[hang, cot].Text)
+                    {
+                        int tam = int.Parse(A[hang + 1, cot].Text) + int.Parse(A[hang, cot].Text);
+                        A[hang + 1, cot].Text = "" + tam;
+                        A[hang, cot].Text = "" + 0;
+                        hang = hang + 1;
+                    }
+                    else if (A[hang + 1, cot].Text == "0")
+                    {
+                        A[hang + 1, cot].Text = A[hang, cot].Text;
+                        A[hang, cot].Text = "" + 0;
+                        hang = hang + 1;
+                    }
+                    else
+                        chamday = true;
+                }
+            }
+            Invalidate();
+
+            KT();
             Invalidate();
         }
 
+        //private void tmrMoving_Tick(object sender, EventArgs e)
+        //{
+        //    if (_objPosition == Position.Left)
+        //    {
 
-        private void tmrMoving_Tick(object sender, EventArgs e)
-        {
-            if (_objPosition == Position.Left)
-            {
+        //        _x -= 10;
 
-                _x -= 10;
+        //        _x -= 5;
 
-                _x -= 5;
-
-            }
-            else if (_objPosition == Position.Right)
-            {
-                _x += 5;
-            }
+        //    }
+        //    else if (_objPosition == Position.Right)
+        //    {
+        //        _x += 5;
+        //    }
 
 
-            else if (_objPosition == Position.Up)
-            {
-                _y -= 5;
-            }
-            else if (_objPosition == Position.Down)
-            {
-                _y += 5;
-            }
+        //    else if (_objPosition == Position.Up)
+        //    {
+        //        _y -= 5;
+        //    }
+        //    else if (_objPosition == Position.Down)
+        //    {
+        //        _y += 5;
+        //    }
 
-            Invalidate();
-        }
+        //    Invalidate();
+        //}
 
         private void Shades_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Left)
-            {
-                _objPosition = Position.Left;
-            }
-            else if (e.KeyCode == Keys.Right)
-            {
-                _objPosition = Position.Right;
-            }
+            //if (e.KeyCode == Keys.Left)
+            //{
+            //    _objPosition = Position.Left;
+            //}
+            //else if (e.KeyCode == Keys.Right)
+            //{
+            //    _objPosition = Position.Right;
+            //}
 
-            else if (e.KeyCode == Keys.Up)
-            {
-                _objPosition = Position.Up;
-            }
-            else if (e.KeyCode == Keys.Down)
-            {
-                _objPosition = Position.Down;
+            //else if (e.KeyCode == Keys.Up)
+            //{
+            //    _objPosition = Position.Up;
+            //}
+            //else if (e.KeyCode == Keys.Down)
+            //{
+            //    _objPosition = Position.Down;
 
-            }
-            Invalidate();
+            //}
+            //Invalidate();
         }
 
 
